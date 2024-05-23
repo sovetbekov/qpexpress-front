a73dccbc1cdf4f2e2704b89289193d3ec87eba37
Talgat <sovvetbekov@gmail.com>
2024-05-23 18:10:43 +0500

    chore: translation

--- src/app/[language]/admin/marketplaces/create/page.tsx
+++ src/app/[language]/admin/marketplaces/create/page.tsx
@@ -5,6 +5,7 @@ import PageWrapper from '@/app/[language]/profile/PageWrapper';
 import Link from 'next/link';
 import Image from 'next/image';
 import AdminMarketplaceForm from '@/app/components/client/MarketplaceForm/AdminMarketplaceForm';
+import { useTranslation } from '@/app/i18n'
 
 export const dynamic = 'force-dynamic';
 
@@ -14,7 +15,9 @@ type Props = {
     }
 }
 
-export default function Page({ params: { language } }: Readonly<Props>) {
+export default async function Page({ params: { language } }: Readonly<Props>) {
+    const {t} = await useTranslation(language, 'marketplace')
+
     return (
         <PageWrapper>
             <div className="md:p-20 flex flex-col">
@@ -23,7 +26,7 @@ export default function Page({ params: { language } }: Readonly<Props>) {
                         <Image src={'/assets/back_arrow.svg'} alt={'back_arrow.svg'} width={24} height={24} />
                     </Link>
                     <p className="md:text-4xl md:font-bold">
-                        Добавить маркетплейс
+                        {t('add_marketplace')}
                     </p>
                 </div>
                 <div className="w-full ">
