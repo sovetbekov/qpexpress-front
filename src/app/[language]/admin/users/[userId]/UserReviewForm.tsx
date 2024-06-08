1f0e7e8fd6f8fdf95d9f2097c142c99fcaded851
Talgat <sovvetbekov@gmail.com>
2024-06-08 20:17:55 +0500

    refactor: approve multiple recipients

--- src/app/[language]/admin/users/[userId]/UserReviewForm.tsx
+++ src/app/[language]/admin/users/[userId]/UserReviewForm.tsx
@@ -23,8 +23,8 @@ export default function UserReviewForm({data: {user, recipients}, language}: Rea
     const dispatch = useAppDispatch()
     const router = useRouter()
 
-    async function onApproveClick() {
-        await toast.promise(acceptRecipient(recipients[0].id).then(
+    async function onApproveClick(recipientId: string) {
+        await toast.promise(acceptRecipient(recipientId).then(
             response => {
                 if (isSuccess(response)) {
                     return response.data
@@ -81,106 +81,104 @@ export default function UserReviewForm({data: {user, recipients}, language}: Rea
                     </div>
                 </div>
             </div>
-            {recipients.length > 0 && (
-                <>
-                    <div className={'flex flex-col gap-5'}>
-                        <h3>
-                            Получатель
-                        </h3>
-                        <div className={'flex flex-wrap flex-row gap-6'}>
-                            <div className={'md:w-[calc(33%-0.51rem)] w-full'}>
-                                <TextInput id={'firstName'}
-                                           label={'Имя'}
-                                           type={'text'}
-                                           className={'md:p-5 p-3 border rounded-full border-black placeholder:text-black w-full'}
-                                           value={recipients[0].firstName}
-                                           readOnly/>
-                            </div>
-                            <div className={'md:w-[calc(33%-0.51rem)] w-full'}>
-                                <TextInput id={'lastName'}
-                                           label={'Фамилия'}
-                                           type={'text'}
-                                           className={'md:p-5 p-3 border rounded-full border-black placeholder:text-black w-full'}
-                                           value={recipients[0].lastName}
-                                           readOnly/>
-                            </div>
-                            <div className={'md:w-[calc(33%-0.51rem)] w-full'}>
-                                <TextInput id={'patronymic'}
-                                           label={'Отчество'}
-                                           type={'text'}
-                                           className={'md:p-5 p-3 border rounded-full border-black placeholder:text-black w-full'}
-                                           value={recipients[0].patronymic}
-                                           readOnly/>
-                            </div>
-                            <div className={'md:w-[calc(33%-0.51rem)] w-full'}>
-                                <TextInput id={'iin'}
-                                           label={'ИИН'}
-                                           type={'text'}
-                                           className={'md:p-5 p-3 border rounded-full border-black placeholder:text-black w-full'}
-                                           value={recipients[0].iin}
-                                           readOnly/>
-                            </div>
-                            <Link className={'md:w-[calc(33%-0.51rem)] w-full'}
-                                  href={`${BACKEND_URL}/v1/files/${recipients[0].documentSideA.id}/download`}
-                                  target={'_blank'}>
-                                <TextInput id={'photo1'}
-                                           label={'Сторона А'}
-                                           type={'text'}
-                                           className={'md:p-5 p-3 border rounded-full border-black text-blue w-full cursor-pointer'}
-                                           value={recipients[0].documentSideA.name}
-                                           readOnly/>
-                            </Link>
-                            <Link className={'md:w-[calc(33%-0.51rem)] w-full'}
-                                  href={`${BACKEND_URL}/v1/files/${recipients[0].documentSideB.id}/download`}
-                                  target={'_blank'}>
-                                <TextInput id={'photo2'}
-                                           label={'Сторона Б'}
-                                           type={'text'}
-                                           className={'md:p-5 p-3 border rounded-full border-black text-blue w-full cursor-pointer'}
-                                           value={recipients[0].documentSideB.name}
-                                           readOnly/>
-                            </Link>
-                            <div className={'md:w-[calc(33%-0.51rem)] w-full'}>
-                                <TextInput id={'district'}
-                                           label={'Город/область'}
-                                           type={'text'}
-                                           className={'md:p-5 p-3 border rounded-full border-black placeholder:text-black w-full'}
-                                           value={`${recipients[0].district}`}
-                                           readOnly/>
-                            </div>
-                            <div className={'md:w-[calc(33%-0.51rem)] w-full'}>
-                                <MaskInput id={'phoneNumber'}
-                                           label={'Номер телефона'}
-                                           format={'+7 (###) ###-##-##'}
-                                           className={'md:p-5 p-3 border rounded-full border-black placeholder:text-black w-full'}
-                                           value={`${recipients[0].phoneNumber}`}
-                                           readOnly/>
-                            </div>
-                            <div className={'md:w-[calc(33%-0.51rem)] w-full'}>
-                                <TextInput id={'address'}
-                                           label={'Адрес'}
-                                           className={'md:p-5 p-3 border rounded-full border-black placeholder:text-black w-full'}
-                                           value={`${recipients[0].address}`}
-                                           readOnly/>
-                            </div>
+            {recipients.length > 0 && recipients.map((recipient, index) => (
+                <div key={recipient.id} className={'flex flex-col gap-5'}>
+                    <h3>
+                        Получатель {index + 1}
+                    </h3>
+                    <div className={'flex flex-wrap flex-row gap-6'}>
+                        <div className={'md:w-[calc(33%-0.51rem)] w-full'}>
+                            <TextInput id={'firstName'}
+                                       label={'Имя'}
+                                       type={'text'}
+                                       className={'md:p-5 p-3 border rounded-full border-black placeholder:text-black w-full'}
+                                       value={recipient.firstName}
+                                       readOnly/>
+                        </div>
+                        <div className={'md:w-[calc(33%-0.51rem)] w-full'}>
+                            <TextInput id={'lastName'}
+                                       label={'Фамилия'}
+                                       type={'text'}
+                                       className={'md:p-5 p-3 border rounded-full border-black placeholder:text-black w-full'}
+                                       value={recipient.lastName}
+                                       readOnly/>
+                        </div>
+                        <div className={'md:w-[calc(33%-0.51rem)] w-full'}>
+                            <TextInput id={'patronymic'}
+                                       label={'Отчество'}
+                                       type={'text'}
+                                       className={'md:p-5 p-3 border rounded-full border-black placeholder:text-black w-full'}
+                                       value={recipient.patronymic}
+                                       readOnly/>
+                        </div>
+                        <div className={'md:w-[calc(33%-0.51rem)] w-full'}>
+                            <TextInput id={'iin'}
+                                       label={'ИИН'}
+                                       type={'text'}
+                                       className={'md:p-5 p-3 border rounded-full border-black placeholder:text-black w-full'}
+                                       value={recipient.iin}
+                                       readOnly/>
+                        </div>
+                        <Link className={'md:w-[calc(33%-0.51rem)] w-full'}
+                              href={`${BACKEND_URL}/v1/files/${recipient.documentSideA.id}/download`}
+                              target={'_blank'}>
+                            <TextInput id={'photo1'}
+                                       label={'Сторона А'}
+                                       type={'text'}
+                                       className={'md:p-5 p-3 border rounded-full border-black text-blue w-full cursor-pointer'}
+                                       value={recipient.documentSideA.name}
+                                       readOnly/>
+                        </Link>
+                        <Link className={'md:w-[calc(33%-0.51rem)] w-full'}
+                              href={`${BACKEND_URL}/v1/files/${recipient.documentSideB.id}/download`}
+                              target={'_blank'}>
+                            <TextInput id={'photo2'}
+                                       label={'Сторона Б'}
+                                       type={'text'}
+                                       className={'md:p-5 p-3 border rounded-full border-black text-blue w-full cursor-pointer'}
+                                       value={recipient.documentSideB.name}
+                                       readOnly/>
+                        </Link>
+                        <div className={'md:w-[calc(33%-0.51rem)] w-full'}>
+                            <TextInput id={'district'}
+                                       label={'Город/область'}
+                                       type={'text'}
+                                       className={'md:p-5 p-3 border rounded-full border-black placeholder:text-black w-full'}
+                                       value={`${recipient.district}`}
+                                       readOnly/>
+                        </div>
+                        <div className={'md:w-[calc(33%-0.51rem)] w-full'}>
+                            <MaskInput id={'phoneNumber'}
+                                       label={'Номер телефона'}
+                                       format={'+7 (###) ###-##-##'}
+                                       className={'md:p-5 p-3 border rounded-full border-black placeholder:text-black w-full'}
+                                       value={`${recipient.phoneNumber}`}
+                                       readOnly/>
+                        </div>
+                        <div className={'md:w-[calc(33%-0.51rem)] w-full'}>
+                            <TextInput id={'address'}
+                                       label={'Адрес'}
+                                       className={'md:p-5 p-3 border rounded-full border-black placeholder:text-black w-full'}
+                                       value={`${recipient.address}`}
+                                       readOnly/>
                         </div>
                     </div>
                     <div className={'flex flex-row gap-5'}>
                         {
-                            recipients[0].status !== 'ACTIVE' &&
+                            recipient.status !== 'ACTIVE' &&
                             <button className={'py-4 px-10 w-[15rem] bg-blue text-white rounded-full'}
-                                    onClick={onApproveClick}>
+                                    onClick={() => onApproveClick(recipient.id)}>
                                 Принять
                             </button>
                         }
                         <button className={'py-4 px-10 w-[15rem] bg-orange text-white rounded-full'} onClick={() => {
-                            dispatch(openModal({modalType: 'denialReason', data: {recipient: recipients[0]}}))
+                            dispatch(openModal({modalType: 'denialReason', data: {recipient}}))
                         }}>
                             Отклонить
                         </button>
                     </div>
-                </>
-            )}
+                </div>
+            ))}
         </div>
     )
 }
