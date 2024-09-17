b54a63359272e2662df3da00fc7f9f3df184bf3a
Talgat Sovetbekov <sovvetbekov@gmail.com>
2024-09-17 21:59:38 +0500

    refactor: add whatsapp link & remove turkey from the title

--- src/app/[language]/layout.tsx
+++ src/app/[language]/layout.tsx
@@ -10,6 +10,7 @@ import 'react-toastify/dist/ReactToastify.css'
 import { dir } from 'i18next'
 import { languages } from '@/app/i18n/settings'
 import ClientComponent from './gmt'
+import { FaWhatsapp } from 'react-icons/fa'
 
 export const metadata: Metadata = {
     title: 'QP Express',
@@ -25,7 +26,6 @@ export async function generateStaticParams() {
     return languages
 }
 
-
 type Props = {
     children: React.ReactNode,
     params: {
@@ -34,29 +34,37 @@ type Props = {
 }
 
 export default function RootLayout({
-                                       children,
-                                       params: {
-                                           language,
-                                       },
-                                   }: Readonly<Props>) {
+    children,
+    params: {
+        language,
+    },
+}: Readonly<Props>) {
     return (
         <html lang={language} dir={dir(language)}>
-        <body>
-        <Providers>
-            <div className={'h-[100dvh] overflow-y-auto overflow-x-hidden scroll-pt-20'}>
-                <ClientComponent />
-                <Header language={language}/>
-                <Navigation language={language}/>
-                <div className={'min-h-[calc(100dvh-5rem)] flex flex-col justify-between'}>
-                    <div className={'pb-10'}>
-                        {children}
+            <body>
+                <Providers>
+                    <div className={'h-[100dvh] overflow-y-auto overflow-x-hidden scroll-pt-20'}>
+                        <ClientComponent />
+                        <Header language={language} />
+                        <Navigation language={language} />
+                        <div className={'min-h-[calc(100dvh-5rem)] flex flex-col justify-between'}>
+                            <div className={'pb-10'}>
+                                {children}
+                            </div>
+                            <Footer language={language} />
+                        </div>
+                        <Modal language={language} />
+                        <a
+                            href="https://api.whatsapp.com/send/?phone=77000888090&text=%D0%97%D0%B4%D1%80%D0%B0%D0%B2%D1%81%D1%82%D0%B2%D1%83%D0%B9%D1%82%D0%B5%21+%D0%AF+%D1%85%D0%BE%D1%82%D0%B5%D0%BB%D0%B0+%D1%81%D0%B4%D0%B5%D0%BB%D0%B0%D1%82%D1%8C+%D0%B7%D0%B0%D0%BA%D0%B0%D0%B7&type=phone_number&app_absent=0"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="whatsapp-button"
+                        >
+                            <FaWhatsapp color="white" size="24px" />
+                        </a>
                     </div>
-                    <Footer language={language}/>
-                </div>
-                <Modal language={language}/>
-            </div>
-        </Providers>
-        </body>
+                </Providers>
+            </body>
         </html>
     )
 }
